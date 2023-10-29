465557831cbbfd16434fa763b570afeaeea34521
Robbe-Vr <437402@student.fontys.nl>
2023-10-29 23:46:35 +0100

    SYWTB-45 Setup Tournaments service project

---
+++ Tournaments-service/SYWTourneyBot.Tournaments.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SYWTourneyBot.Tournaments.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Tournaments-service/SYWTourneyBot.Tournaments/Controllers/Controller.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SYWTourneyBot.Tournaments.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+        public Controller()
+        {
+            
+        }
+    }
+}
