4ec967727249852333f7d8b6eb2ac5032d719a40
Robbe-Vr <437402@student.fontys.nl>
2023-10-30 00:08:03 +0100

    SYWTB-50 Setup Players service project

---
+++ Players-service/SYWTourneyBot.Players/Controllers/Controller.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SYWTourneyBot.Players.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+        public Controller()
+        {
+            
+        }
+    }
+}
---
+++ Players-service/SYWTourneyBot.PlayersTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SYWTourneyBot.Players.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
