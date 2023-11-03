df57af9c86f35794db46e8e2abc6a00d198cfe3b
Robbe-Vr <437402@student.fontys.nl>
2023-11-03 16:26:52 +0100

    SYWTB-64 Create Available Tournaments Repository

---
+++ Tournaments-service/SYWTourneyBot.Tournaments.DAL/Services/Storage/Repositories/AvailableTournamentsRepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SYWTourneyBot.Tournaments.Exchange.DTO.Tournament;
+
+namespace SYWTourneyBot.Tournaments.DAL.Services.Storage.Repositories
+{
+    public class AvailableTournamentsRepo
+    {
+        public AvailableTournamentsRepo() { }
+
+        public IEnumerable<Tournament> All(int page, int limit)
+        {
+            return new List<Tournament>();
+        }
+
+        public IEnumerable<Tournament> Search(string search, string? by, int page, int limit)
+        {
+            return new List<Tournament>();
+        }
+    }
+}
