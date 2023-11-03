4e6a4ff61bac6c73e1559b02bc37c559c7d98b88
Robbe-Vr <437402@student.fontys.nl>
2023-11-03 16:16:13 +0100

    SYWTB-63 Create Available Tournaments Component

---
+++ Tournaments-service/SYWTourneyBot.Tournaments.Core/AvailableTournamentsHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SYWTourneyBot.Tournaments.Exchange.DTO.Tournament;
+
+namespace SYWTourneyBot.Tournaments.Core
+{
+    public class AvailableTournamentsHandler
+    {
+        public AvailableTournamentsHandler() { }
+
+        public IEnumerable<Tournament> All(int page, int limit)
+        {
+            return new List<Tournament>();
+        }
+
+        public IEnumerable<Tournament> Search(string search, string? by, int page, int limit)
+        {
+            return new List<Tournament>();
+        }
+    }
+}
