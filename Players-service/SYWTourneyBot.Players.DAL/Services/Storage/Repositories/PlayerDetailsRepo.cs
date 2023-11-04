4e63b404e10264ee6c8fcd8bcfbe06c8c34199f9
Robbe-Vr <437402@student.fontys.nl>
2023-11-04 20:09:18 +0100

    SYWTB-76 Create Player Details Repository

---
+++ Players-service/SYWTourneyBot.Players.DAL/Services/Storage/Repositories/PlayerDetailsRepo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SYWTourneyBot.Players.Exchange.DTO.Player;
+using SYWTourneyBot.Players.Exchange.DTO.Player.Rank;
+using SYWTourneyBot.Players.Exchange.DTO.Player.Ranks;
+
+namespace SYWTourneyBot.Players.DAL.Services.Storage.Repositories
+{
+    public class PlayerDetailsRepo
+    {
+        public PlayerDetailsRepo()
+        {
+        
+        }
+
+        public IEnumerable<Player> GetAll(int page, int limit)
+        {
+            return new List<Player>();
+        }
+
+        public IEnumerable<Player> Search(string search, string? by, int page, int limit)
+        {
+            return new List<Player>();
+        }
+
+        public Player GetById(string id)
+        {
+            return new Player();
+        }
+
+        public string GetIdByOtherId(string otherId)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ Players-service/SYWTourneyBot.Players.DAL/Services/Storage/Repositories/RanksRepo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SYWTourneyBot.Players.Exchange.DTO.Player.Rank;
+using SYWTourneyBot.Players.Exchange.DTO.Player.Ranks;
+
+namespace SYWTourneyBot.Players.DAL.Services.Storage.Repositories
+{
+    public class RanksRepo
+    {
+        public RanksRepo()
+        {
+        
+        }
+
+        public Ranks GetRanks(string id)
+        {
+            return new Ranks();
+        }
+
+        public GameRank GetGameRank(string id)
+        {
+            return new GameRank();
+        }
+
+        public GameModeRank GetGameModeRank(string id)
+        {
+            return new GameModeRank();
+        }
+    }
+}
