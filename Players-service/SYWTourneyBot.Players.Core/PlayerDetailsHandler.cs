9258927ef6267eb01a3b75bc368a5ff56b4ab1d8
Robbe-Vr <437402@student.fontys.nl>
2023-11-04 20:06:19 +0100

    SYWTB-75 Create Player Details Component

---
+++ Players-service/SYWTourneyBot.Players.Core/PlayerDetailsHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SYWTourneyBot.Players.Exchange.DTO.Player;
+
+namespace SYWTourneyBot.Players.Core
+{
+    public class PlayerDetailsHandler
+    {
+        public PlayerDetailsHandler()
+        {
+
+        }
+
+        public IEnumerable<Player> GetAll(int page, int limit)
+        {
+            return new List<Player>();
+        }
+
+        public IEnumerable<Player> Search(string search, string? by, int page, int limit)
+        {
+            return new List<Player>();
+        }
+
+        public Player GetById(string id)
+        {
+            return new Player();
+        }
+
+        public string GetIdByOtherId(string otherId)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ Players-service/SYWTourneyBot.Players.Core/RanksHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SYWTourneyBot.Players.Exchange.DTO.Player;
+using SYWTourneyBot.Players.Exchange.DTO.Player.Rank;
+using SYWTourneyBot.Players.Exchange.DTO.Player.Ranks;
+
+namespace SYWTourneyBot.Players.Core
+{
+    public class RanksHandler
+    {
+        public RanksHandler()
+        {
+
+        }
+
+        public Ranks GetRanks(string id)
+        {
+            return new Ranks();
+        }
+
+        public GameRank GetGameRank(string id)
+        {
+            return new GameRank();
+        }
+
+        public GameModeRank GetGameModeRank(string id)
+        {
+            return new GameModeRank();
+        }
+    }
+}
